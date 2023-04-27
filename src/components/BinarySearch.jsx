3352f125db1df943f662c087655741e0da7075b4
Alexandr_Ovchar <azalexxx02@gmail.com>
2023-04-27 17:43:05 +0300

    steps 1 second

--- src/components/BinarySearch.jsx
+++ src/components/BinarySearch.jsx
@@ -5,24 +5,36 @@ function BinarySearchComponent() {
   const [numbers, setNumbers] = useState([...Array(100)].map((_, i) => i));
   const [searchValue, setSearchValue] = useState('');
   const [resultIndex, setResultIndex] = useState(-1);
+  const [steps, setSteps] = useState(0); // variable to track number of steps
 
   // Binary search algorithm
   function binarySearch(arr, value) {
     let low = 0;
     let high = arr.length - 1;
-
-    while (low <= high) {
+    let count = 0;
+  
+    function searchInterval() {
       const mid = Math.floor((low + high) / 2);
+      count++;
+  
       if (arr[mid] === value) {
-        return mid;
-      } else if (arr[mid] < value) {
-        low = mid + 1;
+        setSteps(count);
+        setResultIndex(mid);
+      } else if (low <= high) {
+        if (arr[mid] < value) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+        setTimeout(searchInterval, 1000); // delay each iteration by 1 second
       } else {
-        high = mid - 1;
+        setSteps(count);
+        setResultIndex(-1);
       }
     }
-
-    return -1;
+  
+    setTimeout(searchInterval, 1000); // initial delay before starting the search
+  
   }
 
   // Handle search input change
@@ -49,7 +61,9 @@ function BinarySearchComponent() {
       </form>
       <p>Array: {numbers.join(', ')}</p>
       {resultIndex >= 0 ? (
-        <p>Found at index {resultIndex}</p>
+        <p>
+          Found at index {resultIndex}, took {steps} steps
+        </p>
       ) : (
         <p>Not found</p>
       )}
